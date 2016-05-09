fd03569147e4d6fc654d999802d01937eb747998
Benjamin Toll <benjam72@yahoo.com>
2016-05-09 19:47:33 -0400

    ES6ified

--- src/index.js
+++ src/index.js
@@ -1,72 +1,65 @@
 'use strict';
 
-(() => {
-    let crypto = require('crypto'),
-        wordlist = require('./lib/wordlist'),
-        size = 6,
-        diceware;
+const crypto = require('crypto');
+const wordlist = require('./wordlist');
+let size = 6;
 
-    function getKey(n) {
-        let res;
+function getKey(n) {
+    let res;
 
-        if (n < 1) {
-            return '';
-        }
-
-        res = [];
-        // We always want 5-digit keys.
-        n = n || 5;
-        res.push(new Uint32Array(crypto.randomBytes(1))[0] % 6 + 1);
-
-        return res.concat(getKey(--n)).join('');
+    if (n < 1) {
+        return '';
     }
 
-    diceware = Object.create(null, {
-        /**
-         * The default size of the generated passphrase.
-         */
-        size: {
-            get: function () {
-                return size;
-            },
-            set: function (v) {
-                if (v < 1) {
-                    throw new Error('Diceware: Negative passphrase sizes are not allowed.');
-                }
+    res = [];
+    // We always want 5-digit keys.
+    n = n || 5;
+    res.push(new Uint32Array(crypto.randomBytes(1))[0] % 6 + 1);
 
-                size = v;
+    return res.concat(getKey(--n)).join('');
+}
+
+const diceware = Object.create(null, {
+    /**
+     * The default size of the generated passphrase.
+     */
+    size: {
+        get: () => size,
+        set: v => {
+            if (v < 1) {
+                throw new Error('Diceware: Negative passphrase sizes are not allowed.');
             }
-        },
 
-        /**
-         * Either pass in a number to use as the passphrase length or set `diceware.size`
-         * to the preferred default.
-         */
-        getPassphrase: {
-            value: function (n) {
-                let res;
-
-                if (n < 1) {
-                    return '';
-                }
-
-                res = [];
-                n = n || diceware.size;
-                res.push(diceware.getWord());
-
-                return res.concat(diceware.getPassphrase(--n)).join(' ');
-            }
-        },
-
-        getWord: {
-            value: function () {
-                return wordlist[getKey()];
-            }
+            size = v;
         }
-    });
+    },
 
-    diceware.generate = diceware.getPassphrase;
+    /**
+     * Either pass in a number to use as the passphrase length or set `diceware.size`
+     * to the preferred default.
+     */
+    getPassphrase: {
+        value: n => {
+            let res;
 
-    module.exports = diceware;
-})();
+            if (n < 1) {
+                return '';
+            }
+
+            res = [];
+            n = n || diceware.size;
+            res.push(diceware.getWord());
+
+            return res.concat(diceware.getPassphrase(--n)).join(' ');
+        }
+    },
+
+    getWord: {
+        value: () => wordlist[getKey()]
+    }
+});
+
+diceware.generate = diceware.getPassphrase;
+
+module.exports = diceware;
 
